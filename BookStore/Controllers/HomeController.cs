3b90ab32f55e3a36ad1da17e3e1dd4a098f71506
Stephen8500 <73487095+Stephen8500@users.noreply.github.com>
2022-02-15 17:49:48 -0700

    initial commit

---
+++ BookStore/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using BookStore.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        public HomeController()
+        {
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
