5d14ca00e7884fe4cc3bd5f7edb575f4b6c43568
Stephen Williams <williste@byu.edu>
2022-03-28 18:25:34 -0600

    added repos and startup

--- BowlingLeagueApp/Startup.cs
+++ BowlingLeagueApp/Startup.cs
@@ -1,6 +1,8 @@
+using BowlingLeagueApp.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -24,6 +26,13 @@ namespace BowlingLeagueApp
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            services.AddDbContext<BowlersDbContext>(options =>
+            {
+                options.UseMySql(Configuration["ConnectionStrings:BowlersDbConnection"]);
+            });
+
+            services.AddScoped<IBowlersRepository, EFBowlersRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
